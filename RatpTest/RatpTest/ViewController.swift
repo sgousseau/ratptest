ea1347877b6f23adbb51b19a870430c5eb5b6591
sgousseau <sgousseau.externe@solocal.com>
2020-12-19 20:23:48 +0100

    Init project

---
+++ RatpTest/RatpTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RatpTest
+//
+//  Created by Sébastien Gousseau on 19/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
